71ccef4548f99a774b7dfb0ca7a13454d9a05734
Harry Owen <harry@twistedvin.es>
2017-08-21 21:21:23 +0100

    Install grub & bootloader

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+run_in_chroot() {
+  local command="$@"
+  arch-chroot /mnt $command
+}
+
 echo "Beginning installation..."
 echo "pacstrapping using base installation files..."
-pacstrap /mnt base
+pacstrap /mnt base grub
+
+run_in_chroot grub-install --target=i386-pc /dev/sda
+run_in_chroot grub-mkconfig -o /boot/grub/grub.cfg
