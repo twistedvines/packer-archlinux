1817fb41d557f0f9e9cd4bee2b757a2a252ef311
Harry Owen <harry@twistedvin.es>
2017-08-27 19:46:26 +0100

    Suppress configuration output

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -27,11 +27,11 @@ configure_locales() {
 }
 
 configure_keyboard() {
-  echo "KEYMAP=uk" | run_in_chroot tee -a /etc/vconsole.conf
+  echo "KEYMAP=uk" | run_in_chroot tee -a /etc/vconsole.conf > /dev/null
 }
 
 set_hostname() {
-  echo "arch-linux" | run_in_chroot tee /etc/hostname
+  echo "arch-linux" | run_in_chroot tee /etc/hostname > /dev/null
 }
 
 enable_dhcpcd() {
