be635e935483e4c1c50cc723b3fb1330410c174c
Harry Owen <harry@twistedvin.es>
2017-08-31 23:56:58 +0100

    Use DKMS instead of generic drivers
    ... which means we can get rid of my shitty `expect` script too.

--- scripts/install_guest_additions.sh
+++ scripts/install_guest_additions.sh
@@ -2,14 +2,4 @@
 
 echo 'installing guest additions...'
 
-pacstrap /mnt expect
-
-arch-chroot /mnt expect -c \
-'set timeout 60
-spawn pacman -S virtualbox-guest-utils
-expect -re "Enter a number .*:" {
-  after 100 { send -- "2\r" }
-}
-expect {Proceed with installation? [Y/n] }
-send -- "y\r"
-expect eof'
+pacstrap /mnt virtualbox-guest-utils
