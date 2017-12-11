902d35e6078814822c5e634181cf4a3b6d07440c
Harry Owen <harry@twistedvin.es>
2017-12-11 23:41:00 +0000

    Dynamically download ISO from remote
    and checksum it

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 get_project_dir() {
   local project_path
   project_path="${BASH_SOURCE[0]%*/*}"
@@ -12,16 +14,47 @@ get_project_dir() {
 project_dir="$(get_project_dir)"
 build_json="$(cat "${project_dir}/build.json")"
 
-post_processors_head="$1"
-shift
-post_processors_tail="$@"
+add_relevant_post_processors() {
+  post_processors_head="$1"
+  shift
+  post_processors_tail="$@"
 
-jq_select_string=".type == \"$post_processors_head\""
+  jq_select_string=".type == \"$post_processors_head\""
 
-for post_processor in $post_processors_tail; do
-  jq_select_string="$jq_select_string or .type == \"$post_processor\""
-done
+  for post_processor in $post_processors_tail; do
+    jq_select_string="$jq_select_string or .type == \"$post_processor\""
+  done
 
+  echo "$build_json" | jq "[.\"post-processors\"[] | select($jq_select_string)]"
+}
+
+fetch_latest_iso() {
+  local date="$1"
+  local iso_name="archlinux-${date}-x86_64.iso"
+  if ! [ -f "${project_dir}/iso/$iso_name" ]; then
+    find "${project_dir}/iso/"* -type f -exec rm -f {} \; > /dev/null
+    curl -sL "http://mirror.bytemark.co.uk/archlinux/iso/latest/$iso_name" \
+      -o "${project_dir}/iso/$iso_name"
+  fi
+  echo "${project_dir}/iso/$iso_name"
+}
+
+fetch_latest_iso_checksums() {
+  curl -sL 'http://mirror.bytemark.co.uk/archlinux/iso/latest/md5sums.txt' \
+    -o "${project_dir}/iso/md5sums.txt"
+}
+
+get_md5_checksum() {
+  local checksums="$@"
+  echo "$checksums" | grep '.iso' | awk '{print $1}'
+}
+
+iso_date="$(date +%Y.%m.)01"
+iso_path="$(fetch_latest_iso "$iso_date")"
+fetch_latest_iso_checksums
+checksums="$(cat "${project_dir}/iso/md5sums.txt")"
+md5="$(get_md5_checksum "$checksums")"
+post_processors="$(add_relevant_post_processors $@)"
 cd "$project_dir"
-post_processors="$(echo "$build_json" | jq "[.\"post-processors\"[] | select($jq_select_string)]")"
-echo "$build_json" | jq ".\"post-processors\" = $post_processors" | packer build -force -
+echo "$build_json" | jq ".\"post-processors\" = $post_processors" | \
+  packer build -var "iso_checksum=${md5}" -var "local_iso_path=${iso_path}" -force -
