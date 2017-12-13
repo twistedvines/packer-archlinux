ee81088e6d2192cde5feaf58e90675d60488a865
Harry Owen <harry@twistedvin.es>
2017-12-13 22:03:09 +0000

    Add ability to inject custom root password

--- build.sh
+++ build.sh
@@ -5,8 +5,12 @@ set -e
 POST_PROCESSORS=
 
 get_opts() {
-  while getopts 'p:' opt; do
+  while getopts 'p:a' opt; do
     case "$opt" in
+      a)
+        read -s -p 'desired root password: ' DESIRED_ROOT_PASSWORD
+        echo
+        ;;
       p)
         POST_PROCESSORS="${POST_PROCESSORS}${OPTARG} "
         ;;
@@ -38,6 +42,28 @@ add_relevant_post_processors() {
   done
 
   echo "$build_json" | jq "[.\"post-processors\"[] | select($jq_select_string)]"
+}
+
+process_build_json() {
+  local build_json="$1"
+  local post_processors="$2"
+  local variables="$3"
+
+  # reset post-processors (for filtering), but merge variables
+  build_json="$(echo "$build_json" | \
+    jq ".\"post-processors\" = $post_processors")"
+  build_json="$(echo "$build_json" | \
+    jq ".variables = .variables * $variables")"
+  echo "$build_json"
+}
+
+generate_additional_variables() {
+  local vars_json='{}'
+  if [ -n "$DESIRED_ROOT_PASSWORD" ]; then
+    vars_json="$(echo "$vars_json" | \
+      jq ".desired_root_password = \"$DESIRED_ROOT_PASSWORD\"")"
+  fi
+  echo "$vars_json"
 }
 
 fetch_latest_iso() {
@@ -78,13 +104,18 @@ fetch_latest_iso_checksums
 checksums="$(cat "${project_dir}/iso/md5sums.txt")"
 md5="$(get_md5_checksum "$checksums")"
 post_processors="$(add_relevant_post_processors $POST_PROCESSORS)"
+additional_variables="$(generate_additional_variables)"
 
 vm_name="packer-arch-linux-$(date +%Y%m%d%H%M%S)"
+build_json="$(process_build_json \
+  "$(cat "${project_dir}/build.json")" \
+  "${post_processors}" \
+  "${additional_variables}"
+)"
 
 cd "$project_dir"
-echo "$build_json" | jq ".\"post-processors\" = $post_processors" | \
-  packer build \
-    -var "iso_checksum=${md5}" \
-    -var "local_iso_path=${iso_path}" \
-    -var "vm_name=${vm_name}" \
-    -force -
+echo  "$build_json" | packer build \
+  -var "iso_checksum=${md5}" \
+  -var "local_iso_path=${iso_path}" \
+  -var "vm_name=${vm_name}" \
+  -force -
