5259c1961290df0d4184c58555ff2086c1fc8016
Harry Owen <harry@twistedvin.es>
2017-12-13 22:31:22 +0000

    Use simplified method of passing password var

--- build.sh
+++ build.sh
@@ -47,23 +47,11 @@ add_relevant_post_processors() {
 process_build_json() {
   local build_json="$1"
   local post_processors="$2"
-  local variables="$3"
 
   # reset post-processors (for filtering), but merge variables
   build_json="$(echo "$build_json" | \
     jq ".\"post-processors\" = $post_processors")"
-  build_json="$(echo "$build_json" | \
-    jq ".variables = .variables * $variables")"
   echo "$build_json"
-}
-
-generate_additional_variables() {
-  local vars_json='{}'
-  if [ -n "$DESIRED_ROOT_PASSWORD" ]; then
-    vars_json="$(echo "$vars_json" | \
-      jq ".desired_root_password = \"$DESIRED_ROOT_PASSWORD\"")"
-  fi
-  echo "$vars_json"
 }
 
 fetch_latest_iso() {
@@ -104,13 +92,11 @@ fetch_latest_iso_checksums
 checksums="$(cat "${project_dir}/iso/md5sums.txt")"
 md5="$(get_md5_checksum "$checksums")"
 post_processors="$(add_relevant_post_processors $POST_PROCESSORS)"
-additional_variables="$(generate_additional_variables)"
 
 vm_name="packer-arch-linux-$(date +%Y%m%d%H%M%S)"
 build_json="$(process_build_json \
   "$(cat "${project_dir}/build.json")" \
-  "${post_processors}" \
-  "${additional_variables}"
+  "${post_processors}"
 )"
 
 cd "$project_dir"
@@ -118,4 +104,5 @@ echo  "$build_json" | packer build \
   -var "iso_checksum=${md5}" \
   -var "local_iso_path=${iso_path}" \
   -var "vm_name=${vm_name}" \
+  -var "desired_root_password=${DESIRED_ROOT_PASSWORD}" \
   -force -
